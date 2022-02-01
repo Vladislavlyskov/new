11b3fc28e92bc10c9945fdb237a3fb0799863580
user <vladislavlyskov@gmail.com>
2022-02-01 19:24:13 +0300

    test

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-print('hello')
-print('world')
+def funk(a):
+    while True:
+        b = input('Enter ')
+        if b == 'stop':
+            break
+        a = a + int(b)
+        print(a)
+funk(0)
+
+def func(d):
+    while True:
+        c = input('Enter ')
+        if c == 'stop':
+            break
+        d = d * int(c)
+        print(d)
+func(1)
