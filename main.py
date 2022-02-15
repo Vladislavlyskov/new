663de124a6118362c790425535a40f328a29f1e8
user <vladislavlyskov@gmail.com>
2022-02-15 20:03:30 +0300

    test

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-list = ['доход', 'обход', 'восток']
-for i in list:
-    if i == i[::-1]:
-        print(i,  '- Palindrome')
-    else:
-        print('Not Palindrome')
+# # zadanie 10.6
+count = 0
+eq = True
+with open("test.txt", "r", encoding = "utf-8") as my_file, open("out.txt", "r", encoding = "utf-8") as new_file:
+    for a1, a2 in zip(my_file, new_file):
+        count += 1
+        if a1 != a2:
+            eq = False
+            break
+print(f"Нет отличий") if eq else print(f"Отличается строка {count}")
+#
+# # zadanie 10.1
+my_file = open('test.txt')
+print(my_file.readline())
+while True:
+    line = my_file.readline()
+    if not line:
+        break
 
-list1 = ['доход', 'обход', 'восток']
-list2 = [i for i in list1 if i == i[::-1]]
-print(i, '- Palindrome')
+    print(line)
+#
+#
+# my_file.close()
+#
+with open('test.txt') as my_file:
+    lines = my_file.readlines()
+    print(lines[4])
 
+#zadanie 10.3
+with open('test.txt', 'a') as my_file:
+    my_file.writelines([input('enter ') + '\n' for i in range(3)])
 
+# zadanie 10.5
+with open('test.txt') as my_file, open('out.txt', 'w') as new_file, open('out1.txt', 'w') as new_file1:
+    lines = my_file.readlines()
+    new_file.writelines(lines[::2])
+    new_file1.writelines(lines[1::2])
