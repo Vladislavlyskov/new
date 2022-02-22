022d6c7f69022a60ecb3274333286255ce39c849
user <vladislavlyskov@gmail.com>
2022-02-22 19:24:27 +0300

    test

--- main.py
+++ main.py
@@ -1,67 +1,25 @@
-# class A:
-#     def do_somethink(selfself):
-#         print('AA')
-# class B(A):
-#     def do_somethink(self):
-#         print('BB')
-# obj = B()
-# obj.do_somethink()
-#
-#
-# class A:
-#     def do_somethink(selfself):
-#         print('AA')
-# class B(A):
-#     def do_somethink(self):
-#         super().do_somethink()
-#         print('BB')
-# obj = B()
-# obj.do_somethink()
-#
-# class A:
-#     def __init__(self, a):
-#         self.a = a
-# class B:
-#     def __init__(self, a, b):
-#         super().__init__(a)
-#         self.b = b
+class Pet():
+    name = ''
+    def __init__(self, name):
+        self.name = name
 
-#
-# class Calculator:
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
-#
-#     def summ(self):
-#       print(self.a + self.b)
-#
-#     def otn(self):
-#         print(self.a - self.b)
-#     def delenie(self):
-#         print(self.a / self.b)
-#     def umn(self):
-#         print(self.a * self.b)
-# obj = Calculator(10, 5)
-# obj.summ()
-# obj.otn()
-# obj.delenie()
-# obj.umn()
 
-# class Calc:
-#     def sum(self, a, b):
-#         print(a+b)
-#     def minus(self, a, b):
-#         print(a-b)
-# c = Calc()
-# # c.sum(1, 5)
-# print(dir(Calc))
+class Cat(Pet):
+    def make_noise(self):
+        print('Мяу')
 
-class SomeClass:
-    var = 'text'
-    def __init__(self, new_var):
-        self.new_var = new_var
-a = SomeClass(1)
-b = SomeClass(2)
-print(a.new_var, a.var)
-print(b.new_var, b.var)
+class Dog(Pet):
+    def make_noize(self):
+        print('Гав')
+
+myAnimal = Pet('Tom')
+print('his name', myAnimal.name)
+
+tom = Cat('Tom')
+tom.name
+tom.make_noise()
+
+Tom = Dog('Tom')
+Tom.name
+Tom.make_noize()
 
